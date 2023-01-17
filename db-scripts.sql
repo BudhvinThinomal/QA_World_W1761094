f59952d3a46bfb3b4cc7ce7113a44d9314f877ce
Budhvin Thinomal <61777208+BudhvinThinomal@users.noreply.github.com>
2023-01-17 12:26:10 +0530

    database changes done

--- db-scripts.sql
+++ db-scripts.sql
@@ -36,3 +36,40 @@ CREATE TABLE `answer_details` (
     FOREIGN KEY (`username`) REFERENCES `user_details`(`username`),
     FOREIGN KEY (`questionID`) REFERENCES `question_details`(`questionID`)
 );
+
+CREATE TABLE `question_details` (
+	`questionID` INT(10) AUTO_INCREMENT NOT NULL,
+    `questionTitle` VARCHAR(150) NOT NULL,
+    `questionDescription` LONGTEXT NOT NULL,
+    `createdTime` TIMESTAMP NOT NULL,
+    `lastModified` TIMESTAMP NOT NULL,
+    `upVotes` INT(255) NOT NULL DEFAULT 0,
+    `downVotes` INT(255) NOT NULL DEFAULT 0,
+    `username` VARCHAR(40) NOT NULL,
+    `views` INT(255) NOT NULL DEFAULT 0,
+    `tags` LONGTEXT NOT NULL,
+    PRIMARY KEY (`questionID`),
+    FOREIGN KEY (`username`) REFERENCES `user_details`(`username`)
+);
+
+CREATE TABLE `question_votes` (
+	`voteID` INT(10) AUTO_INCREMENT NOT NULL,
+    `like` INT(10) NOT NULL,
+    `dislike` INT(10) NOT NULL,
+    `username` VARCHAR(40) NOT NULL,
+    `questionID` INT(10) NOT NULL,
+    PRIMARY KEY (`voteID`),
+    FOREIGN KEY (`username`) REFERENCES `user_details`(`username`),
+    FOREIGN KEY (`questionID`) REFERENCES `question_details`(`questionID`)
+);
+
+CREATE TABLE `answer_votes` (
+	`voteID` INT(10) AUTO_INCREMENT NOT NULL,
+    `like` INT(10) NOT NULL,
+    `dislike` INT(10) NOT NULL,
+    `username` VARCHAR(40) NOT NULL,
+    `answerID` INT(10) NOT NULL,
+    PRIMARY KEY (`voteID`),
+    FOREIGN KEY (`username`) REFERENCES `user_details`(`username`),
+    FOREIGN KEY (`answerID`) REFERENCES `answer_details`(`answerID`)
+);
